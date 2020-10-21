8e26775d3466db0c4e3ae11e3055c6530394dc42
Dylan Turner <dylantdmt@gmail.com>
2020-10-21 16:46:08 +0000

    [HW07] Commented out broken I2C stuff

--- hw07/blynktest/setup.sh
+++ hw07/blynktest/setup.sh
@@ -2,5 +2,5 @@ export BLYNK_AUTH='TY24DjD6DOiO7tUavJ7oWwTUZ7Bz8myL'
 
 # If useing BMP085 Temp/Pressure sensor
 
-I2C=/sys/class/i2c-adapter/i2c-2
-echo bmp085 0x77 > $I2C/new_device
+#I2C=/sys/class/i2c-adapter/i2c-2
+#echo bmp085 0x77 > $I2C/new_device
