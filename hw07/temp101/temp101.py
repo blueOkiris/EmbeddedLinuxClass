1eaca6bb7c4cc8a68efa996e34e7d2f6fe8e985a
Dylan Turner <dylantdmt@gmail.com>
2020-10-21 13:16:42 -0400

    [HW07] Added timer to read temperature and update value

--- hw07/temp101/temp101.py
+++ hw07/temp101/temp101.py
@@ -1,4 +1,5 @@
 import blynklib
+import blynktimer
 import busio
 import Adafruit_BBIO.GPIO as GPIO
 
@@ -17,13 +18,6 @@ GPIO.output(LED, 1)
 button = 'P9_11'
 GPIO.setup(button, GPIO.IN)
 # print("button: " + str(GPIO.input(button)))
-
-# Set up temp101 sensor
-i2c = busio.I2C(board.SCL, board.SDA)
-addr = 0x49
-rawTemp = bytearray(2)
-#i2c.readfrom_into(addr, rawTemp)
-#temp = rawToCelsius(rawTemp)
 
 # Get the autherization code (See setup.sh)
 BLYNK_AUTH = 'TY24DjD6DOiO7tUavJ7oWwTUZ7Bz8myL'#os.getenv('BLYNK_AUTH')
@@ -50,7 +44,25 @@ def pushed(channel):
     blynk.virtual_write(10, 255*state)  # Virtual LED: 255 max brightness
 
 # This is a non-blocking event 
-GPIO.add_event_detect(button, GPIO.BOTH, callback=pushed) 
+GPIO.add_event_detect(button, GPIO.BOTH, callback=pushed)
+
+# Set up temp101 sensor
+i2c = busio.I2C(board.SCL, board.SDA)
+addr = 0x49
+rawTemp = bytearray(2)
+#i2c.readfrom_into(addr, rawTemp)
+#temp = rawToCelsius(rawTemp)
+
+# Set up blynk timer
+timer = blynktimer.Timer()
+WRITE_EVENT_PRINT_MSG = "[WRITE_VIRTUAL_WRITE] Pin: V{} Value: '{}'"
+@timer.register(vpin_num=9, interval=4, run_once=False)
+def write_to_virtual_pin(vpin_num=1):
+    i2c.readfrom_into(addr, rawTemp)
+    value = rawToCelsius(rawTemp)
+    print(WRITE_EVENT_PRINT_MSG.format(vpin_num, value))
+    blynk.virtual_write(vpin_num, value)
 
 while True:
     blynk.run()
+    timer.run()
