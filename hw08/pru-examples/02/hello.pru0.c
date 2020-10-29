c712238ff4646aa0154effba05337c75bebf3c93
Dylan Turner <dylantdmt@gmail.com>
2020-10-29 03:59:29 +0000

    [HW08] Got it working infinitely and with no delay

--- hw08/pru-examples/02/hello.pru0.c
+++ hw08/pru-examples/02/hello.pru0.c
@@ -9,29 +9,23 @@ volatile register unsigned int __R31;
 #define P8_12	(1 << 12)
 
 void main(void) {
-	int i;
+    uint32_t *gpio1 = (uint32_t *)GPIO1;
+    
+    /* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
+    CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
-	uint32_t *gpio1 = (uint32_t *)GPIO1;
-	
-	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
-	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
-
-	for(i=0; i<1000; i++) {
-		gpio1[GPIO_SETDATAOUT]   = P8_12;	// The the USR3 LED on
-
-		__delay_cycles(500000000/5);    	// Wait 1/2 second
-
-		gpio1[GPIO_CLEARDATAOUT] = P8_12;
-
-		__delay_cycles(500000000/5); 
-
-	}
-	__halt();
+    while(1) {
+        gpio1[GPIO_SETDATAOUT]   = P8_12;
+        __delay_cycles(0);
+        gpio1[GPIO_CLEARDATAOUT] = P8_12;
+        __delay_cycles(0); 
+    }
+    //__halt();
 }
 
 // Turns off triggers
 #pragma DATA_SECTION(init_pins, ".init_pins")
 #pragma RETAIN(init_pins)
 const char init_pins[] =  
-	"/sys/class/leds/beaglebone:green:usr3/trigger\0none\0" \
-	"\0\0";
+    "/sys/class/leds/beaglebone:green:usr3/trigger\0none\0" \
+    "\0\0";
