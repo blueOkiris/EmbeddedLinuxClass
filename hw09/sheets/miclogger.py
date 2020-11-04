610852ace7f2cea359dd7c75e3b1752a1a58aa11
Dylan Turner <dylantdmt@gmail.com>
2020-11-04 15:48:51 -0500

    [HW09] Went back to old version

--- hw09/sheets/miclogger.py
+++ hw09/sheets/miclogger.py
@@ -1,57 +1,52 @@
 #!/usr/bin/env python3
 
 from __future__ import print_function
-import pickle
-import os.path
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+from httplib2 import Http
+from oauth2client import file, client, tools
+import time, sys, speech_recognition
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+# If modifying these scopes, delete the file token.json.
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1kBSJDmjRba7FHl_Y5KjrU7RE55wsCS9AJkZoBocIjUE'
-SAMPLE_RANGE_NAME = 'A2'
+SPREADSHEET_ID = '1kBSJDmjRba7FHl_Y5KjrU7RE55wsCS9AJkZoBocIjUE'
+RANGE_NAME = 'A2'
 
 def main():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+    # Shows basic usage of the Sheets API and writes values to a spreadsheet.
+    store = file.Storage('tokenPython.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('sheets', 'v4', http = creds.authorize(Http()))
 
-    service = build('sheets', 'v4', credentials=creds)
+    # Call the Sheets API && compute timestamp and pass the first two arguments
+    values = [
+        [
+            time.time() / 60 / 60 / 24 + 25569 - 4 / 24,
+            sys.argv[1], sys.argv[2]
+        ]
+    ]
+    body = { 'values': values }
+    result = service.spreadsheets().values().append(
+        spreadsheetId = SPREADSHEET_ID, range = RANGE_NAME,
+        
+        #  How the input data should be interpreted.
+        valueInputOption='USER_ENTERED',
+        
+        # How the input data should be inserted.
+        # insertDataOption = 'INSERT_ROWS',
+        
+        body = body
+    ).execute()
+    
+    updates = result.get('updates', [])
+    # print(updates)
 
-    # Call the Sheets API
-    sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=SAMPLE_RANGE_NAME).execute()
-    values = result.get('values', [])
-
-    if not values:
-        print('No data found.')
-    else:
-        print('Name, Major:')
-        for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s, %s' % (row[0], row[4]))
+    if not updates:
+        print('Not updated')
 
 # Retrieve audio from a microphone and return a string represtending the speech
 def listen():
