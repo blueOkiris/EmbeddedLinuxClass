9e44d0861522e5e38e2ffb28b2e822765f912264
Dylan Turner <dylantdmt@gmail.com>
2020-11-04 19:20:36 -0500

    [HW09] Removed temperature stuff

--- hw09/thingspeak/miclogger.py
+++ hw09/thingspeak/miclogger.py
@@ -36,9 +36,6 @@ def listen():
         return '~Unknown error'
     return response
 
-#TMP101a='/sys/class/i2c-adapter/i2c-2/2-0048/hwmon/hwmon0/'
-#TMP101b='/sys/class/i2c-adapter/i2c-2/2-0049/hwmon/hwmon1/'
-
 # Get the key (See setup.sh)
 key = os.getenv('THING_KEY', default="")
 if(key == ""):
@@ -49,20 +46,6 @@ url = 'https://api.thingspeak.com/update'
 print(url)
 
 while(1):
-    #f = open(TMP101a+'temp1_input', "r")
-    #temp1=f.read()[:-1]     # Remove trailing new line
-    # Convert from mC to C
-    #temp1 = int(temp1)/1000
-    #f.close()
-    #print("temp1: " + str(temp1))
-
-    #f = open(TMP101b+'temp1_input', "r")
-    #temp2=f.read()[:-1]
-    #temp2 = int(temp2)/1000
-    #f.close()
-    #print("temp2: " + str(temp2))
-    
-    # Get speech input
     speech = listen()
     print('You said: ' + speech)
         
